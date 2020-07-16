32d1e6a651625831ff10ca316d7a215b22fde746
Edgar <aguirre.edgar567@gmail.com>
2020-07-16 02:40:46 -0500

    changed app.js, app.css

--- src/App.js
+++ src/App.js
@@ -11,14 +11,34 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link-1"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <a
+          className="App-link-2"
+          href="https://www.freecodecamp.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Web Development with Free Code Camp
+        </a>
+        <a
+          className="App-link-3"
+          href="https://www.codecademy.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CodeCademy is Paid version of website like Free Code Camp
+        </a>
+
+
       </header>
+
+
     </div>
   );
 }
